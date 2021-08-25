7b3eee398b1aa0c16ff4e2f6c46b64b4191be27e
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-25 22:04:26 +0000

    client setup: w02d3

--- play.js
+++ play.js
@@ -1,22 +1,2 @@
-const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: '50542',
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
-
-console.log("Connecting ...");
-connect();
